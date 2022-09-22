2c6cdb8aea986fc64cd153bbe20a0a9a6badd4f0
Nguyen Ngoc Cong <congnn@behemoth.vn>
2022-09-22 15:22:06 +0700

    add some migrations and models

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_092226) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_081824) do
   create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
     t.boolean "correct", default: false
@@ -51,6 +51,31 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_092226) do
     t.bigint "question_id", null: false
   end
 
+  create_table "submission_answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "submission_detail_id"
+    t.integer "question_id"
+    t.integer "answer_id"
+    t.integer "input_answer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "submission_details", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "submission_id"
+    t.integer "question_id"
+    t.integer "question_position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "submissions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "uuid"
+    t.integer "exam_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "uuid"
     t.integer "duration"
@@ -58,6 +83,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_092226) do
     t.integer "lesson_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "date_of_birth"
+    t.boolean "is_active"
+    t.datetime "actived_at"
+    t.string "activation_token"
+    t.integer "role"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
